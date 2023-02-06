c60d271a222a7a2431a7e271e2b4eea578e0fc6b
Akutsang <mymac@MYs-MacBook-Pro.local>
2023-02-06 18:43:15 +0100

    all DOM elements targeted

--- project-Calculator/stylesheet.js
+++ project-Calculator/stylesheet.js
@@ -1,20 +1,34 @@
 // DOM ELEMENTS
 const hour = document.querySelector(".hour");
 const minute = document.querySelector(".minute");
-const clear = document.querySelector(".ac")
-const percent = document.querySelector("percent")
-const pminus = document.querySelector(".pm")
+const clear = document.querySelector(".ac");
+const percent = document.querySelector("percent");
+const pminus = document.querySelector(".pm");
 
-const division = document.querySelector(".division")
-const multiplication = document.querySelector(".multiplication")
-const subtraction = document.querySelector(".subtraction")
-const addition = document.querySelector(".addition")
-const equal = document.querySelector(".equal")
+const division = document.querySelector(".division");
+const multiplication = document.querySelector(".multiplication");
+const subtraction = document.querySelector(".subtraction");
+const addition = document.querySelector(".addition");
+const equal = document.querySelector(".equal");
+
+const decimal = document.querySelector(".decimal");
+const zero = document.querySelector(".number-0");
+const one = document.querySelector(".number-1");
+const two = document.querySelector(".number-2");
+const three = document.querySelector(".number-3");
+const four = document.querySelector(".number-4");
+const five = document.querySelector(".number-5");
+const six = document.querySelector(".number-6");
+const seven = document.querySelector(".number-7");
+const eight = document.querySelector(".number-8");
+const nine = document.querySelector(".number-9");
+
+const numberArray = [
+    zero, one, two, three, four, five, six, seven, eight, nine
+];
 
 
-
-
-// set time 
+// set time (to achieve that, we will need some methods such as; Date(), .getHours(), .getMinutes())
 const updateTime = () => {
         const currentTime = new Date();
 
@@ -29,5 +43,6 @@ if (currentHour > 12) {
 }
 setInterval(updateTime, 1000);
 updateTime();
+
 
 
